0e19cd4bedfb2409728e0d862f315185aef5e1ce
Chetan Aggarwal <46553269+chetan2002@users.noreply.github.com>
2020-11-06 22:53:21 +0530

    Update targetSumInArray.cpp

--- DailyPracticeProblem/11_6_2020/targetSumInArray.cpp
+++ DailyPracticeProblem/11_6_2020/targetSumInArray.cpp
@@ -1,7 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//this is the best approach for this question - time complexity O(n) and space complexity O(n)
 bool wasMyNumberPresentMap(vector<int> number , int n , int k){
+    //for every value , check if (target-value) is present in the map ,
+    // if yes return true
+    //else insert the value in map in hope to find it's complement afterwards
     unordered_map<int,int> mp;
     for(auto c: number){
         if(mp.find(k-c)!=mp.end()){
@@ -13,7 +18,12 @@ bool wasMyNumberPresentMap(vector<int> number , int n , int k){
     return false;
 }
 
+//this is a good approach || time complexity O(n) and space complexity O(1)
 bool wasMyNumberPresentTwoPointer(vector<int> number , int n , int k){
+    //sort the list and then start moving pointers from both ends , 
+    //if sum of the elements pointed by the pointers is equal to target then return true , 
+    //if sum is greater then we need a smaller element so move the right pointer towards left 
+    //else if our target sum is smaller then move left pointer right
     sort(number.begin() , number.end());
     int i=0;int j=n-1;
     while(i<j){
@@ -30,7 +40,9 @@ bool wasMyNumberPresentTwoPointer(vector<int> number , int n , int k){
     return false;
 }
 
+//brute force , naive approach || time complexity O(n2) and space complexity O(1)
 bool wasMyNumberPresentNaive(vector<int> number , int n , int k){
+    //for each element , check all other elements if they hit the target 
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             if(number[i]+number[j]==k){
@@ -40,30 +52,39 @@ bool wasMyNumberPresentNaive(vector<int> number , int n , int k){
     }
     return false;
 }
+
+
 int main(){
+    //input n- number of element in array 
     int n;
     cin>>n;
     vector<int> v;
+    //input the array of n elements
     for(int i=0;i<n;i++){
         int val;
         cin>>val;
         v.push_back(val);
 
     }
+    //input the target to search
     int k;
     cin>>k;
+
+    //call for naive approach
     if(wasMyNumberPresentNaive(v,n,k)){
         cout<<"Naive approach says numbers were present"<<endl;
     }else{
         cout<<"Naive approach says numbers weren't present"<<endl;
     }
 
+    //call for two pointer approach
     if(wasMyNumberPresentTwoPointer(v,n,k)){
         cout<<"Two Pointer approach says numbers were present"<<endl;
     }else{
         cout<<"Two Pointer approach says numbers weren't present"<<endl;
     }
 
+    //call for map based solution
     if(wasMyNumberPresentMap(v,n,k)){
         cout<<"Mapping approach says numbers were present"<<endl;
     }else{
