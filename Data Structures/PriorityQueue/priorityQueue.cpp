49a54a86fa4e8397ab4b7e75b7fb76ba40437ab3
chetan2002 <46553269+chetan2002@users.noreply.github.com>
2021-02-01 11:50:14 +0530

    1-2-21

--- Data Structures/PriorityQueue/priorityQueue.cpp
+++ Data Structures/PriorityQueue/priorityQueue.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Minheap{
+    public:
+        vector<int> v;
+        Minheap(){
+            v.push_back(-1);    //done bcz we want to avoid 0th index
+        }
+
+        void push(int d){
+
+        }
+};
+
 int main(){
     
     return 0;
